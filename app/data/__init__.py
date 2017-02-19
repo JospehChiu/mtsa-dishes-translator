7d6eac193883c13c3936b059eb5a76c58b1452c3
Joseph <b99501002@gmail.com>
2017-02-18 22:36:46 -0500

    Basic cold joke bot

--- app/data/__init__.py
+++ app/data/__init__.py
@@ -1,8 +1,10 @@
 from .parsedishes import add_dishes
 from .parsevegetables import add_vegetables
 from .parsepokemon import add_pokemons
+from .parsejokes import add_jokes
 
 def prepare_data(dryrun=True):
     add_dishes(dryrun)
     add_vegetables(dryrun)
     add_pokemons(dryrun)
+    add_jokes(dryrun)
